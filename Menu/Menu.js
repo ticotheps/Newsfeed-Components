aecbf5567e8db983c74b1e808d7406730df2d229
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-30 15:34:07 -0500

    Used the menuButton reference to to add a click handler that calls the toggleMenu() function

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,8 @@
 
-// const toggleMenu = () => {
-//   // Toggle the "menu--open" class on your menu refence. 
-// }
+const toggleMenu = () => {
+  // Toggle the "menu--open" class on your menu reference. 
+  console.log("The toggle-function of the menu has been triggered due to a click on the .menu-button <img> element!");
+}
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelectorAll('.menu');
@@ -12,3 +13,4 @@ const menuButton = document.querySelector('.menu-button');
 console.log(menuButton);
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', () => toggleMenu());
